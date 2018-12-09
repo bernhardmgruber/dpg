d107f69238b561fb6dc236c0a852dae05b6966e8
dixxi <dixxi1@gmx.net>
2018-12-09 02:44:31 +0100

    added write and file open primitives

--- src/IO.h
+++ src/IO.h
@@ -32,11 +32,39 @@ auto readVector(std::istream& is, std::size_t count) {
 	return v;
 }
 
-inline auto readTextFile(const fs::path& filename) {
-	std::string content;
-	std::ifstream file(filename);
+template<typename T>
+void write(std::ostream& is, const T& t) {
+	is.write(reinterpret_cast<const char*>(&t), sizeof(T));
+}
+
+template<typename T>
+void writeVector(std::ostream& is, const std::vector<T>& v) {
+	is.write(reinterpret_cast<const char*>(v.data()), sizeof(T) * v.size());
+}
+
+inline auto openFileIn(const fs::path& filename, int flags = 0) {
+	std::ifstream file(filename, flags);
 	if (!file)
 		throw std::ios::failure("Failed to open file " + filename.string() + " for reading");
+	return file;
+}
+
+inline void createDirectories(const fs::path& filename) {
+	if (filename.has_parent_path())
+		create_directories(filename.parent_path());
+}
+
+inline auto openFileOut(const fs::path& filename, int flags = 0) {
+	createDirectories(filename);
+	std::ofstream file(filename, flags);
+	if (!file)
+		throw std::ios::failure("Failed to open file " + filename.string() + " for writing");
+	return file;
+}
+
+inline auto readTextFile(const fs::path& filename) {
+	std::string content;
+	auto file = openFileIn(filename);
 	file.seekg(std::ios::end);
 	content.reserve(file.tellg());
 	file.seekg(std::ios::beg);
